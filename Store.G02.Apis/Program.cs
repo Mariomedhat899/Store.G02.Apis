2cf70f3759462188f1b77831f129f17a1f2b4cc2
Mariomedhat899 <mariomedhat899@gmail.com>
2025-10-25 00:17:22 +0300

    Data Seeding

--- Store.G02.Apis/Program.cs
+++ Store.G02.Apis/Program.cs
@@ -1,14 +1,20 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Store.G02.Domain.Contracts;
+using Store.G02.Persistence;
 using Store.G02.Persistence.Data.Contexts;
+using System.Threading.Tasks;
 
 namespace Store.G02.Apis
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<IDBInitializer, DBInitalizer>();
 
             builder.Services.AddDbContext<StoreDBContext>(options =>
             {
@@ -24,6 +30,15 @@ namespace Store.G02.Apis
 
             var app = builder.Build();
 
+            // Initialize the database Ask From CLR
+            #region Using IServiceScope to create a scope To Initalize DB
+           using var scope =   app.Services.CreateScope();
+
+
+          var DBInitalizer =  scope.ServiceProvider.GetRequiredService<IDBInitializer>();
+            await DBInitalizer.InitalizeAsync();
+
+            #endregion
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
