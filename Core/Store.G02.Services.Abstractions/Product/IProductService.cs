fdb8c6eaae13c1668bb0a1b3d01b2e78b41ba1ce
Mariomedhat899 <mariomedhat899@gmail.com>
2025-10-25 14:23:47 +0300

    Product Services And Product Unit

---
+++ Core/Store.G02.Services.Abstractions/Product/IProductService.cs
@@ -0,0 +1,23 @@
+﻿using Store.G02.Shared.Dtos.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services.Abstractions.Product
+{
+    public interface IProductService
+    {
+       Task<IEnumerable<ProductResponse>> GetAllProductsAsync();
+
+       Task<ProductResponse> GetProductByIdAsync(int Id);
+
+        Task<IEnumerable<BrandTypeResponse>> GetAllBrandsAsync();
+
+        Task<IEnumerable<BrandTypeResponse>> GetAllTypesAsync();
+
+
+
+    }
+}
