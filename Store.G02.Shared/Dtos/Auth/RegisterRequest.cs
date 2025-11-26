a2d756f31b1cce8b62e0f90f0291fc8a060d6238
Mariomedhat899 <mariomedhat899@gmail.com>
2025-11-26 12:20:18 +0200

    Auth Modifications

--- Store.G02.Shared/Dtos/Auth/RegisterRequest.cs
+++ Store.G02.Shared/Dtos/Auth/RegisterRequest.cs
@@ -9,12 +9,12 @@ namespace Store.G02.Shared.Dtos.Auth
 {
     public class RegisterRequest
     {
-        [EmailAddress]
         public string UserName { get; set; }
         public string DisplayName { get; set; }
 
         public string PhoneNumber { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
